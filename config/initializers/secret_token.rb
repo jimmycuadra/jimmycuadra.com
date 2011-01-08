62121ff3adb1f79c0882cbb7af3482aeb8042a42
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-08 14:57:52 -0800

    add Gemfile.lock, update secret token env name

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-JimmycuadraCom::Application.config.secret_token = ENV['JIMMYCUADRA_SECRET_TOKEN']
+JimmycuadraCom::Application.config.secret_token = ENV['SECRET_TOKEN']
