2abb24512906e8c2122849f8a03fdebec545727f
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-09 04:49:27 -0800

    add textile helper method using RedCloth

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source :rubygems
 gem 'rails', '3.0.3'
 gem 'friendly_id', '~> 3.1.8'
 gem 'formtastic', '~> 1.2.3'
+gem 'RedCloth', '~> 4.2.3'
 
 group :development, :test do
   gem 'sqlite3-ruby', :require => 'sqlite3'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def textile(text)
+    text.gsub!(/^@@@ ?(\w*)\r?\n(.+?)\r?\n@@@\r?$/m) do |match|
+      lang = $1.empty? ? nil : $1
+      "\n<notextile>" + $2 + "</notextile>"
+    end
+
+    RedCloth.new(text).to_html.html_safe
+  end
 end
