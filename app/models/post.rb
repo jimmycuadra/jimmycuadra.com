6244d9a6c727c0fff1d2e47ac3f65c30d7bef57b
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-08 20:24:28 -0800

    add validations and friendly id to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :title, :body
+  has_friendly_id :title, :use_slug => true, :approximate_ascii => true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,28 @@
 require 'spec_helper'
 
 describe Post do
+  before(:each) do
+    @post = Factory.build(:post)
+  end
+
+  it "saves valid records" do
+    @post.should be_valid
+  end
+
+  it "requires a title" do
+    @post.title = nil
+    @post.valid?
+    @post.should have(1).error_on(:title)
+  end
+
+  it "requires a body" do
+    @post.body = nil
+    @post.valid?
+    @post.should have(1).error_on(:body)
+  end
+
+  it "generates a slug when saved" do
+    @post.save
+    @post.cached_slug.should_not be_nil
+  end
 end
