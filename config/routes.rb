1111268b7562d2e8f78f908ad8c7ba2f842a875a
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-09 02:05:47 -0800

    add posts#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 JimmycuadraCom::Application.routes.draw do
+  resources :posts
+
   root :to => "posts#index"
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,7 +4,7 @@ describe PostsController do
   render_views
 
   describe "#index" do
-    it "renders index template" do
+    it "renders the index template" do
       get :index
       response.should render_template(:index)
     end
@@ -13,6 +13,22 @@ describe PostsController do
       recent_posts = [Factory(:post), Factory(:post), Factory(:post)]
       get :index
       assigns(:posts).should == recent_posts.reverse
+    end
+  end
+
+  describe "#show" do
+    before(:each) do
+      Factory(:post)
+    end
+
+    it "renders the show template" do
+      get :show, :id => Post.first.to_param
+      response.should render_template(:show)
+    end
+
+    it "displays the post" do
+      get :show, :id => Post.first.id
+      assigns(:post).should == Post.first
     end
   end
 end
