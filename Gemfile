4672ec3d9ff2e117c3cefeeaaecf83972544f477
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-09 04:57:35 -0800

    process code blocks with coderay

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.0.3'
 gem 'friendly_id', '~> 3.1.8'
 gem 'formtastic', '~> 1.2.3'
 gem 'RedCloth', '~> 4.2.3'
+gem 'coderay', '~> 0.9.6'
 
 group :development, :test do
   gem 'sqlite3-ruby', :require => 'sqlite3'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def textile(text)
     text.gsub!(/^@@@ ?(\w*)\r?\n(.+?)\r?\n@@@\r?$/m) do |match|
       lang = $1.empty? ? nil : $1
-      "\n<notextile>" + $2 + "</notextile>"
+      "\n<notextile>" + CodeRay.scan($2, lang).div(:css => :class) + "</notextile>"
     end
 
     RedCloth.new(text).to_html.html_safe
