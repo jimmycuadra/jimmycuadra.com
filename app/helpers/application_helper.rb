aca0fb076bf33266845c309f415065ea020cabe1
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-09 17:47:00 -0800

    add helper method for nav buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def nav_to(link, path, controller_name)
+    content_tag :li, :class => (params[:controller] == controller_name ? "current" : nil) do
+      content_tag :div do
+        link_to link, path, :class => "awesome"
+      end
+    end
+  end
+
   def textile(text)
     text.gsub!(/^@@@ ?(\w*)\r?\n(.+?)\r?\n@@@\r?$/m) do |match|
       lang = $1.empty? ? nil : $1
