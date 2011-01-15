7982dbd5e35e1f0cdce4e2eecc49661b4942b3ff
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-15 01:41:28 -0800

    fix hash lookup of url when creating a user from twitter auth

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -12,7 +12,7 @@ class AuthenticationsController < ApplicationController
       user.authentications.build(:provider => omniauth["provider"], :uid => omniauth["uid"])
       user.username = omniauth["user_info"]["name"] || omniauth["user_info"]["nickname"]
       user.avatar = omniauth["user_info"]["image"] if omniauth["user_info"]["image"]
-      user.url = omniauth["user_info"]["urls"]["Website"] || omniauth["user_info"]["Twitter"] if omniauth["user_info"]["urls"]["Website"] || omniauth["user_info"]["Twitter"]
+      user.url = omniauth["user_info"]["urls"]["Website"] || omniauth["user_info"]["urls"]["Twitter"] if omniauth["user_info"]["urls"]["Website"] || omniauth["user_info"]["urls"]["Twitter"]
       if user.save
         session[:user_id] = user.id
         redirect_to root_path, :notice => "You are now signed in."
