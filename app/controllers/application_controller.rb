b9ab838f9955aa3f06ffd0abe0d7f8a569b2bbd7
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-15 00:41:05 -0800

    add authentications#create case for creating new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def current_user
-    false
+    @current_user ||= session[:current_user]
   end
   helper_method :current_user
 end
--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,2 +1,14 @@
 class AuthenticationsController < ApplicationController
+  def create
+    omniauth = env["omniauth.auth"]
+    authentication = Authentication.find_by_provider_and_uid(omniauth["provider"], omniauth["uid"])
+    if authentication
+    else
+      user = User.new
+      user.authentications.build(:provider => omniauth["provider"], :uid => omniauth["uid"])
+      user.save!
+      session[:current_user] = user.id
+      redirect_to root_path, :notice => "You are now signed in."
+    end
+  end
 end
