aa66853fa0c184ec11b8540a062d8c34da781865
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-15 01:23:26 -0800

    fill in user details when creating a new user via twitter

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,10 +4,15 @@ class AuthenticationsController < ApplicationController
     authentication = Authentication.find_by_provider_and_uid(omniauth["provider"], omniauth["uid"])
     if authentication
       session[:current_user] = authentication.user.id
-      redirect_to root_path, :notice => "You are now signed in."
+      redirect_to root_path, :notice => "Welcome back! You are now signed in."
+    elsif current_user
+      redirect_to root_path, :notice => "Adding another authorization is NYI."
     else
       user = User.new
       user.authentications.build(:provider => omniauth["provider"], :uid => omniauth["uid"])
+      user.username = omniauth["user_info"]["name"] || omniauth["user_info"]["nickname"]
+      user.avatar = omniauth["user_info"]["image"] if omniauth["user_info"]["image"]
+      user.url = omniauth["user_info"]["urls"]["Website"] || omniauth["user_info"]["Twitter"] if omniauth["user_info"]["urls"]["Website"] || omniauth["user_info"]["Twitter"]
       if user.save
         session[:current_user] = user.id
         redirect_to root_path, :notice => "You are now signed in."
