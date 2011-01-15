015ff46a2d0d9d5e8e4e4bb46756a19817eb3fe0
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-14 22:04:53 -0800

    use a custom formatting helper for comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,10 @@
 module CommentsHelper
+  def format_comment(comment)
+    comment = h(comment) # escape user input
+    comment.gsub!("\n\n", "</p><p>") # split paragraphs
+    comment.gsub!("<p></p>", "") # remove empty paragraphs
+    comment.gsub!(/\n(?=.)/, "<br />") # add breaks
+    comment.chop! if comment[-1] == "\n" # strip trailing newline
+    "<p>#{auto_link_urls(comment)}</p>".html_safe
+  end
 end
