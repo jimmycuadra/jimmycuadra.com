9d1102940e7fb38b7fdeafdd77db171d2279607c
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-15 00:48:24 -0800

    add more cases to authentications#create

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,12 +3,17 @@ class AuthenticationsController < ApplicationController
     omniauth = env["omniauth.auth"]
     authentication = Authentication.find_by_provider_and_uid(omniauth["provider"], omniauth["uid"])
     if authentication
+      session[:current_user] = authentication.user.id
+      redirect_to root_path, :notice => "You are now signed in."
     else
       user = User.new
       user.authentications.build(:provider => omniauth["provider"], :uid => omniauth["uid"])
-      user.save!
-      session[:current_user] = user.id
-      redirect_to root_path, :notice => "You are now signed in."
+      if user.save
+        session[:current_user] = user.id
+        redirect_to root_path, :notice => "You are now signed in."
+      else
+        redirect_to root_path, :notice => "Validation failed."
+      end
     end
   end
 end
