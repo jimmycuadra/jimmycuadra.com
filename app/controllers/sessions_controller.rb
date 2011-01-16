0ee78acc8f2d16ef006b070126697a16b82e7490
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-16 00:33:46 -0800

    fall back to root_path redirection if login or logout are hit directly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    return redirect_to :back, :notice => "You are already logged in." if current_user
+    return redirect_to (request.env["HTTP_REFERER"] || root_path), :notice => "You are already logged in." if current_user
 
     session[:return_to] = request.env["HTTP_REFERER"] || root_path
     redirect_to "/auth/#{params[:provider]}"
@@ -8,6 +8,6 @@ class SessionsController < ApplicationController
 
   def destroy
     reset_session
-    redirect_to :back, :notice => "You are now logged out."
+    redirect_to (request.env["HTTP_REFERER"] || root_path), :notice => "You are now logged out."
   end
 end
