145f0932a9ccf6dc9b81bc9633e7088cfeece57b
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-15 22:56:32 -0800

    move user creation logic to model

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,18 +1,22 @@
 class AuthenticationsController < ApplicationController
   def create
+    if current_user
+      return redirect_to root_path, :notice => "Adding another authorization is NYI."
+    end
+
     omniauth = env["omniauth.auth"]
+
     authentication = Authentication.find_by_provider_and_uid(omniauth["provider"], omniauth["uid"])
+
     if authentication
       session[:user_id] = authentication.user.id
       redirect_to root_path, :notice => "Welcome back! You are now logged in."
-    elsif current_user
-      redirect_to root_path, :notice => "Adding another authorization is NYI."
     else
-      user = User.new
-      user.authentications.build(:provider => omniauth["provider"], :uid => omniauth["uid"])
-      user.username = omniauth["user_info"]["name"] || omniauth["user_info"]["nickname"]
-      user.avatar = omniauth["user_info"]["image"] if omniauth["user_info"]["image"]
-      user.url = omniauth["user_info"]["urls"]["Website"] || omniauth["user_info"]["urls"]["Twitter"] if omniauth["user_info"]["urls"]["Website"] || omniauth["user_info"]["urls"]["Twitter"]
+      user = case omniauth["provider"]
+      when "twitter"
+        User.build_from_twitter(omniauth)
+      end
+
       if user.save
         session[:user_id] = user.id
         redirect_to root_path, :notice => "You are now logged in."
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ActiveRecord::Base
 
   has_many :authentications
   has_many :comments
+
+  def self.build_from_twitter(omniauth)
+    user = build_from_auth(omniauth)
+    user.username = omniauth["user_info"]["name"] || omniauth["user_info"]["nickname"]
+    user.avatar = omniauth["user_info"]["image"] if omniauth["user_info"]["image"]
+    user.url = omniauth["user_info"]["urls"]["Website"] || omniauth["user_info"]["urls"]["Twitter"] if omniauth["user_info"]["urls"]["Website"] || omniauth["user_info"]["urls"]["Twitter"]
+    user
+  end
+
+  private
+
+  def self.build_from_auth(omniauth)
+    user = User.new
+    user.authentications.build(:provider => omniauth["provider"], :uid => omniauth["uid"])
+    user
+  end
 end
