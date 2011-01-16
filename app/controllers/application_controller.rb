34995e9f8b57087b77120dfd5b6ad2bc92e22d76
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-15 22:12:03 -0800

    add admin? method to control display of admin actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  helper_method :current_user, :admin?
+
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
+
+  def admin?
+    current_user && current_user.id == 1
+  end
 end
