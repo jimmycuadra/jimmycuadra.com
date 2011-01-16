b2dec489548304f6ceb3cdd59310f578213f36a0
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-16 03:56:14 -0800

    add specs for authentications controller

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,6 +1,6 @@
 class AuthenticationsController < ApplicationController
   def create
-    omniauth = env["omniauth.auth"]
+    omniauth = request.env["omniauth.auth"]
 
     authentication = Authentication.find_by_provider_and_uid(omniauth["provider"], omniauth["uid"])
 
@@ -14,14 +14,10 @@ class AuthenticationsController < ApplicationController
         User.build_from_twitter(omniauth)
       end
 
-      if user.save
-        session[:user_id] = user.id
-        redirect_to (session[:return_to] || root_path), :notice => "You are now logged in."
-        session[:return_to] = nil
-      else
-        redirect_to (session[:return_to] || root_path), :notice => "Validation failed."
-        session[:return_to] = nil
-      end
+      user.save!
+      session[:user_id] = user.id
+      redirect_to (session[:return_to] || root_path), :notice => "You are now logged in."
+      session[:return_to] = nil
     end
   end
 end
