40bad338cd735c933b30bb67fd4ae429a6b01d12
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-16 21:17:24 -0800

    favor gravatar over stored avatar URL

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,12 @@
 module UsersHelper
   def user_avatar(user)
-    puts "user.avatar: #{user.avatar}"
-    puts "user.email: #{user.email}"
-
     default_avatar_url = "http://www.gravatar.com/avatar/00000000000000000000000000000000?s=48"
 
     avatar_url = case
-    when user.avatar
-      user.avatar
     when user.email
       "http://gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email.downcase)}.png?s=48"
+    when user.avatar
+      user.avatar
     end
 
     "<img src=\"#{avatar_url || default_avatar_url}\" width=\"48\" height=\"48\" alt=\"#{user.name}'s avatar\" />".html_safe
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe UsersHelper do
   describe "#user_avatar" do
-    it "uses the avatar attribute if available" do
-      user = mock(User, :avatar => "/path/to/avatar", :email => "bongo@example.com", :name => "Bongo")
+    it "uses the avatar attribute if available and email is not" do
+      user = mock(User, :avatar => "/path/to/avatar", :email => nil, :name => "Bongo")
       user_avatar(user).should include("/path/to/avatar")
     end
 
     it "uses gravatar if email is available" do
-      user = mock(User, :avatar => nil, :email => "bongo@example.com", :name => "Bongo")
+      user = mock(User, :avatar => "/path/to/avatar", :email => "bongo@example.com", :name => "Bongo")
       user_avatar(user).should include(Digest::MD5.hexdigest(user.email.downcase))
     end
 
