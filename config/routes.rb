c4345c4e38a4981f1ce3a8dc4f75ea3ccb145f14
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-25 23:03:30 -0800

    remove old auth-related routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,20 +7,11 @@ JimmycuadraCom::Application.routes.draw do
     resources :comments, :only => [:create, :destroy]
   end
   resources :tags, :only => [:index, :show]
-  resources :users, :only => [:edit, :update]
 
   # static pages
   match "projects" => "projects#index", :as => :projects
   match "about" => "about#index", :as => :about
 
-  # omniauth callback
-  match "auth/:provider/callback" => "authentications#create"
-  match "auth/failure" => "authentications#failure"
-
   # sessions
-  match "login/:provider" => "sessions#create", :as => :login
-  match "logout" => "sessions#destroy", :as => :logout
-
-  # users
-  match "profile" => "users#edit", :as => :profile
+  resources :sessions, :only => [:new, :create, :destroy]
 end
