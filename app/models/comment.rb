229d1ee95cfeb4fcbfb74e0e2cc04cd7d1e6bf9b
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-25 22:04:09 -0800

    customize error messages for invalid email and URL in comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   validates_presence_of :name, :email, :comment
-  validates_format_of :email, :with => /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/, :allow_blank => true
-  validates_format_of :url, :with => /^(#{URI::regexp(%w(http https))})$/i, :allow_blank => true
+  validates_format_of :email, :with => /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/, :allow_blank => true, :message => "must be a valid email address"
+  validates_format_of :url, :with => /^(#{URI::regexp(%w(http https))})$/i, :allow_blank => true, :message => "must be valid URL"
 
   belongs_to :post
 end
