7d7d70dca41c8b244c8e1e7c95880bb626984a5d
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-25 21:40:20 -0800

    remove sessions logic for now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,2 @@
 class SessionsController < ApplicationController
-  before_filter :require_user, :only => :destroy
-
-  def create
-    return redirect_to (request.env["HTTP_REFERER"] || root_path), :notice => "You are already logged in." if current_user
-
-    session[:return_to] = request.env["HTTP_REFERER"] || root_path
-    redirect_to "/auth/#{params[:provider]}"
-  end
-
-  def destroy
-    reset_session
-    redirect_to (request.env["HTTP_REFERER"] || root_path), :notice => "You are now logged out."
-  end
 end
