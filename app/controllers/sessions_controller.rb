6aeb9b31cebd830bbe6d4050afcf7756a7a199bd
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-25 23:22:33 -0800

    add sessions#new and sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if password_match?(params[:password])
+      session[:admin] = true
+      redirect_to root_path, :notice => "You are now logged in."
+    else
+      flash.now[:notice] = "Invalid password."
+      render :new
+    end
+  end
+
+  private
+
+  def password_match?(password)
+    password == ENV['ADMIN_PASSWORD']
+  end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,4 +1,39 @@
 require 'spec_helper'
 
 describe SessionsController do
+  render_views
+
+  describe "#new" do
+    it "should render the new template" do
+      get :new
+      response.should render_template(:new)
+    end
+  end
+
+  describe "#create" do
+    context "with a valid password" do
+      before(:each) do
+        controller.stub(:password_match?).and_return(true)
+        post :create
+      end
+
+      it "logs in as an admin" do
+        controller.admin?.should == true
+      end
+
+      it "redirects to the root with a flash" do
+        response.should redirect_to(root_path)
+        flash[:notice].should include("logged in")
+      end
+    end
+
+    context "with an invalid password" do
+      it "renders the new template with a flash" do
+        controller.stub(:password_match?).and_return(false)
+        post :create
+        response.should render_template(:new)
+        flash[:notice].should include("Invalid")
+      end
+    end
+  end
 end
