9de5b8eb9628392cd66f453ef031f8fc0f896921
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-25 22:26:10 -0800

    add admin? and require_admin methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :admin?
+
+  def admin?
+    session[:admin] == true
+  end
+
+  private
+
+  def require_admin
+    redirect_to root_path, :notice => "You are not authorized." unless admin?
+  end
 end
