b589c55968c43473cd0d070b35fe215ecc0a8a24
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-01-27 16:42:58 -0800

    covert underscores to dashes in tag slugs

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -1,5 +1,10 @@
 module ActsAsTaggableOn
   class Tag
     has_friendly_id :name, :use_slug => true, :approximate_ascii => true
+
+    def normalize_friendly_id(text)
+      text.gsub!("_", "-")
+      super
+    end
   end
 end
---
+++ spec/lib/acts_as_taggable_on.rb
@@ -0,0 +1,22 @@
+# coding: utf-8
+require 'spec_helper'
+
+describe ActsAsTaggableOn::Tag do
+  it "generates a slug when saved" do
+    subject.name = 'tag'
+    subject.save
+    subject.cached_slug.should_not be_nil
+  end
+
+  it "approximates ASCII in generated slugs" do
+    subject.name = "My résumé"
+    subject.save
+    subject.cached_slug.should == "my-resume"
+  end
+
+  it "converts underscores to dashes in generated slugs" do
+    subject.name = "this_has_underscores"
+    subject.save
+    subject.cached_slug.should == "this-has-underscores"
+  end
+end
