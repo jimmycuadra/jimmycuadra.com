4f5255fe6234d5d086d6c14e47bfb86116c175d0
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-03-02 00:14:47 -0800

    add mailers group to simplecov report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ SimpleCov.start do
   add_group "Models", "/app/models"
   add_group "Controllers", "/app/controllers"
   add_group "Helpers", "/app/helpers"
+  add_group "Mailers", "/app/mailers"
   add_group "Configuration", "/config"
 end
 require File.expand_path("../../config/environment", __FILE__)
