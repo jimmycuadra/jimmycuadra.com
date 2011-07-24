f3215605f9e51a2f9adbcf8a931015833ac49197
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-07-23 22:55:58 -0700

    safebuffer fix for 3.0.9

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,6 @@
 module CommentsHelper
   def format_comment(comment)
-    comment = h(comment) # escape user input
+    comment = h(comment).to_str # escape user input
     comment.gsub!("\r", "") # strip \r
     comment.gsub!("\n\n", "</p><p>") # split paragraphs
     comment.gsub!("<p></p>", "") # remove empty paragraphs
