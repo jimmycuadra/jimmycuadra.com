a9b751d830ff59154437cafe9b011a13e86dccb7
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-06-27 23:46:33 -0700

    Use better delimiters for email regex.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   attr_accessible :name, :email, :url, :comment
 
   validates_presence_of :name, :email, :comment
-  validates_format_of :email, :with => /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/, :allow_blank => true, :message => "must be a valid email address"
+  validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\z/, :allow_blank => true, :message => "must be a valid email address"
 
   belongs_to :post
 
